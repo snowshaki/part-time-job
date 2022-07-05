46af4e74d3239a0b4ccc1876866a52a54cbf7e63
JeongAnNa <rib040211@gmail.com>
2022-07-05 14:33:30 +0900

    Update

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const route = {
+  indexRouter: require('./routes/index'),
+}
+const port = 3000;
 
 var app = express();
 
@@ -19,8 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', route.indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +38,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => console.log(`part-time-job app listening on port ${port}`));
 
 module.exports = app;
